950b206d102335dffcf55cd5cb7e09373e2117ee
Krzysztof Szyper <krzysztof.szyper@gmail.com>
2020-04-25 17:44:00 +0200

    Updated Action baseline

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,28 @@ set -e
 # Return code
 RET_CODE=0
 
+echo "Inputs:"
+echo "  foobar: ${INPUT_FOOBAR}"
+
+## Require github_token
+#if [[ -z "${INPUT_GITHUB_TOKEN}" ]]; then
+#  # shellcheck disable=SC2016
+#  MESSAGE='Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".'
+#  echo "[ERROR] ${MESSAGE}"
+#  exit 1
+#fi
+
 # Run main action
-echo "[INFO] Env var BAR: ${BAR}"
-echo "[INFO] Input var BAZ: ${INPUT_BAZ}"
+echo "[INFO] Input var BAZ: ${INPUT_FOOBAR}"
 RET_CODE=$?
 
 # Finish
-echo "::set-output name=foobar::${INPUT_BAZ}"
+echo "::set-output name=barfoo::${INPUT_FOOBAR}"
 if [[ ${RET_CODE} != "0" ]]; then
-  echo " "
-  echo "[ERROR] Check log for errors."
-  echo " "
+  echo -e "\n[ERROR] Check log for errors."
   exit 1
 else
   # Pass in other cases
-  echo " "
-  echo "[INFO] No errors found."
-  echo " "
+  echo -e "\n[INFO] No errors found."
   exit 0
 fi
